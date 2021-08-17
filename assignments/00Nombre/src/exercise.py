51a34ddf7bf252640d68a765a913e54c91a34781
Víctor Manuel Bátiz Beltrán <vbatiz@gmail.com>
2021-08-17 17:48:51 +0000

    Agregando caluclo de area de una esfera

--- assignments/00Nombre/src/exercise.py
+++ assignments/00Nombre/src/exercise.py
@@ -5,6 +5,8 @@ def main():
     os.system("clear")
     radio=float(input("Dame el valor del radio: "))
     volumen = 4/3 * math.pi * math.pow(radio,3)
+    area = 4*math.pi*radio ** 2
     print(f"El volumen de la esfera de radio {radio} es: {volumen} unidades cúbicas")
+    print(f"El área de la esfera de radio {radio} es: {area} unidades cuadradas")
 if __name__=='__main__':
     main()
