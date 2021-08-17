44af7da71e1f8bb66d1209ed8c1b9e5b2475d583
Víctor Manuel Bátiz Beltrán <vbatiz@gmail.com>
2021-08-17 17:44:20 +0000

    Ejercicio de esfera resuelto

--- assignments/00Nombre/src/exercise.py
+++ assignments/00Nombre/src/exercise.py
@@ -3,10 +3,8 @@ import os
 def main():
     #escribe tu código abajo de esta línea
     os.system("clear")
-    lado=float(input("Dame el valor del lado del cuadrado: "))
-    perimetro = 4 * lado
-    area = math.pow(lado,2)
-    print(f"El perímetro del cuadrado de lado {lado} es: {perimetro}")
-    print(f"El área del cuadrado de lado {lado} es: {area}")
+    radio=float(input("Dame el valor del radio: "))
+    volumen = 4/3 * math.pi * math.pow(radio,3)
+    print(f"El volumen de la esfera de radio {radio} es: {volumen} unidades cúbicas")
 if __name__=='__main__':
     main()
